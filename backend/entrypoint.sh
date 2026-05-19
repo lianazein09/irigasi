ff0912ca51e4d09926213d1a588a8fc6bb3114db
AlvinSetyaPranata <comand.alvin@gmail.com>
2026-05-19 07:09:12 +0700

    upadte

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 set -eu
 
-DB_ENGINE_NORMALIZED=$(printf '%s' "${DB_ENGINE:-mysql}" | tr '[:upper:]' '[:lower:]')
-
-if [ "$DB_ENGINE_NORMALIZED" = "postgres" ] || [ "$DB_ENGINE_NORMALIZED" = "postgresql" ]; then
-  python /app/ensure_postgres_schema.py
-fi
-
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
